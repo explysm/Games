fb81fe39fa85166b9601904da5aa840013bdcc99
explysm <joshualtrost@gmail.com>
2025-09-06 03:34:16 -0400

    Uodated game to have CaseOh dialog and Mobile Controls.
    Early Access Release is coming soon

--- In_Development/Queso/Scenes/main.gd
+++ In_Development/Queso/Scenes/main.gd
@@ -9,11 +9,13 @@ extends Node3D
 @onready var Caseoh = $Caseoh
 @onready var audio = $Area3D/AudioStreamPlayer3D
 @onready var continue1 = $CanvasLayer/Control/continue
+@onready var MobileControls = $CanvasLayer3
 
 var shown = false
 func _ready():
+	MobileControls.visible = false
 	Caseoh.visible = false
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 	Label1.visible = false
 	Label2.visible = false
 	Label3.visible = false
@@ -42,3 +44,24 @@ func _on_area_3d_2_body_entered(body: Node3D) -> void:
 	Caseoh.visible = true
 	audio.play()
 	shown = true
+	if shown == true:
+		Label2.visible = true
+		LBG.visible = true
+		await get_tree().create_timer(3.0).timeout
+		Label2.visible = false
+		Label3.visible = true
+		await get_tree().create_timer(3.0).timeout
+		Label3.visible = false
+		Label4.visible = true
+		await get_tree().create_timer(3.0).timeout
+		Label4.visible = false
+		Label5.visible = true
+		
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		MobileControls.visible = true
+	if toggled_on == false:
+		MobileControls.visible = false
+	pass # Replace with function body.
