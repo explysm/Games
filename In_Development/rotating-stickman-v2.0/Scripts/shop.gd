90d6b9e7364f10f28761a5b23712a122911069eb
explysm <joshualtrost@gmail.com>
2025-09-08 20:36:32 -0500

    added multipliers to Rotating Stickman v2.0

--- In_Development/rotating-stickman-v2.0/Scripts/shop.gd
+++ In_Development/rotating-stickman-v2.0/Scripts/shop.gd
@@ -1,24 +1,48 @@
 extends CanvasLayer
 
+## Hoodies 
 @onready var BuyBlue = $BGS/Hoodies/Buttons/BuyBlue
 @onready var BoughtBlue = $BGS/Hoodies/Buttons/BoughtBlue
+
+## Multipliers
+@onready var x2 = $BGS/Multipliers/Buttons/Buy2x
+@onready var x3 = $BGS/Multipliers/Buttons/Buy3x
+
+## Pocket Stickman
 @onready var stickman = $BGS/RegularStickman
+
+##Tabs
+@onready var hoodies = $BGS/Hoodies
+@onready var hoodieButtons = $BGS/Hoodies/Buttons
+
+@onready var multipliers = $BGS/Multipliers
+
+## Main functions
 func _ready():
+## Hoodies -
 	if v.blue == false:
 		BoughtBlue.visible = false
 	else:
 		BuyBlue.visible = false
 		BoughtBlue.visible = true
+## Multipliers -
+	if v.multiplier <1:
+		x2.visible = true
+		x3.visible = false
+	elif v.multiplier == 1:
+		x2.visible = false
+		x3.visible = true
+	elif v.multiplier == 2:
+		x3.visible = false
+		x2.visible = false
 
 func _process(delta):
 	if v.hoodie == 0:
 		stickman.animation = "default"
 	elif v.hoodie == 2:
 		stickman.animation = "blue"
-func _on_resume_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-
+## Buy functions Hoodies
 func _on_buy_blue_pressed() -> void:
 	if v.money >199:
 		v.hoodie = 2
@@ -30,6 +54,31 @@ func _on_buy_blue_pressed() -> void:
 func _on_bought_blue_pressed() -> void:
 	v.hoodie = 2
 
+## Buy functions multipliers
+func _on_buy_2x_pressed() -> void:
+	if v.money >449:
+		v.multiplier = 1
+		x2.visible = false
+		x3.visible = true
+func _on_buy_3x_pressed() -> void:
+	if v.money >1449:
+		v.multiplier = 2
+		x3.visible = false
 
+## Etc
 func _on_default_pressed() -> void:
 	v.hoodie = 0
+	
+## Side Buttons
+func _on_multipliers_pressed() -> void:
+	hoodies.visible = false
+	hoodieButtons.visible = false
+	
+	multipliers.visible = true
+func _on_hoodies_pressed() -> void:
+	hoodies.visible = true
+	hoodieButtons.visible = true
+	
+	multipliers.visible = false
+func _on_resume_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
--- In_Development/rotating-stickman-v2.0/Scripts/variables.gd
+++ In_Development/rotating-stickman-v2.0/Scripts/variables.gd
@@ -1,7 +1,9 @@
 extends Node
 
-var money : int = 0
+var money : int = 1000000
 
 var hoodie : int = 0
 
 var blue : bool = false
+
+var multiplier : int = 0
