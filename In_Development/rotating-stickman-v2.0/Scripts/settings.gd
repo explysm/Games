a6ef7d02082876eebb6d2d6433551b0c069d9715
explysm <joshualtrost@gmail.com>
2025-09-09 01:01:01 -0500

    Temporarily disabled virtual joystick. Coming up with a fix soon

--- In_Development/rotating-stickman-v2.0/Scripts/settings.gd
+++ In_Development/rotating-stickman-v2.0/Scripts/settings.gd
@@ -5,25 +5,25 @@ extends CanvasLayer
 @onready var Itch2 = $Social/Itch2
 
 func _ready():
-	hide()
-	Discord.visible = false
-	Discord2.visible = false
-	Itch.visible = false
-	Itch2.visible = false
+    hide()
+    Discord.visible = false
+    Discord2.visible = false
+    Itch.visible = false
+    Itch2.visible = false
 
 func _process(delta):
-	 pass
+     pass
 func _on_resume_btn_pressed() -> void:
-	hide()
-	hide()
-	Discord.visible = false
-	Discord2.visible = false
-	Itch.visible = false
-	Itch2.visible = false
+    hide()
+    hide()
+    Discord.visible = false
+    Discord2.visible = false
+    Itch.visible = false
+    Itch2.visible = false
 
 func _on_settings_btn_pressed() -> void:
-	show()
-	Discord.visible = true
-	Discord2.visible = true
-	Itch.visible = true
-	Itch2.visible = true
+    show()
+    Discord.visible = true
+    Discord2.visible = true
+    Itch.visible = true
+    Itch2.visible = true
